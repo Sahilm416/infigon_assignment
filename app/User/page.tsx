efe4a21c56e39310b50f0416a3897abab1d8b144
sahil <sahilmulani501@gmail.com>
2023-11-16 18:47:49 +0530

    fixed type error while exporting

--- app/User/page.tsx
+++ app/User/page.tsx
@@ -1,42 +1,46 @@
 "use client"
-import React from "react";
-import {useEffect,useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Link from "next/link";
-const User = ({ isLoggedIn, token }: { isLoggedIn: boolean; token: string }) => {
-  const [createdAt, setCreatedAt] = useState("");
-  useEffect(()=>{
-     if(isLoggedIn){
-       getUser()
-     }
-  },[isLoggedIn])
 
- 
-  const getUser = async ()=>{
+interface UserProps {
+  isLoggedIn: boolean;
+  token: string;
+}
+
+const User: React.FC<UserProps> = ({ isLoggedIn, token }) => {
+  const [createdAt, setCreatedAt] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      getUser();
+    }
+  }, [isLoggedIn]);
+
+  const getUser = async () => {
     try {
-        const response = await axios.get('https://dev.api.infigon.app/user/get-profile', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-       setCreatedAt(response.data.createdAt)
+      const response = await axios.get('https://dev.api.infigon.app/user/get-profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCreatedAt(response.data.createdAt);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-        
-      } catch (err) {
-        console.log(err)
-      }
-  }
   return (
-    <div className=" w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center">
       {isLoggedIn ? (
-        <div className=" flex flex-col justify-center items-center">
-          <p>Welcome sahil</p>
-          <p>Your account is created at {createdAt}</p>
+        <div className="flex flex-col justify-center items-center">
+          <p>Welcome Sahil</p>
+          {createdAt && <p>Your account is created at {createdAt}</p>}
         </div>
       ) : (
-        <div className="flex flex-col justify-center items-center ">
-        <p>Protected route Please login first</p>
-        <Link className=" text-blue-400" href="/">To login</Link>
+        <div className="flex flex-col justify-center items-center">
+          <p>Protected route. Please login first.</p>
+          <Link href="/">To login</Link>
         </div>
       )}
     </div>
